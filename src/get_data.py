85744161338be1bf46f8cf6b5646134db0da3582
ashgodbole <ashleshagodbole9@gmail.com>
2021-05-06 17:23:04 +0530

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -26,6 +26,9 @@ def get_data(config_path):
 
 if __name__=="__main__":
     args=argparse.ArgumentParser()
-    args.add_argument("--config",default="params.yaml")
+    args.add_argument("--config",default="C:/Users/ashle/Documents/qualitywine/params.yaml")
     parsed_args=args.parse_args()
     data=get_data(config_path=parsed_args.config)
+
+
+
