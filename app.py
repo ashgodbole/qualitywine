0858a82449841d22b0fc180b6817ed15c6d1b1ce
ashgodbole <ashleshagodbole9@gmail.com>
2021-05-09 20:38:18 +0530

    updated prediction service

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import numpy as np
 from prediction_service import prediction
 
-params_path="params.yaml"
+# params_path="params.yaml"
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
@@ -14,30 +14,7 @@ template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def predict(data):
-    config = read_params(params_path)
-    model_die_path =config["webapp_model_dir"]
-    model = joblib.load("model_dir_path")
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response =predict()
-        response ={"respon":response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Something went wrong!! Try again later!"}
-        return error
-
+#
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -45,19 +22,23 @@ def index():
     if request.method == "POST":
         try:
             if request.form:
-                dict_req = dict(request.form)
-                response = prediction.form_response(dict_req)
-                return render_template("index.html", response=response)
+                data_req =dict(request.form)
+                response =prediction.form_response(data_req)
+                return render_template("index.html",response=response)
+
             elif request.json:
-                response = prediction.api_response(request.json)
+                response=prediction.api_response(request.json)
                 return jsonify(response)
+
+
+            #
 
         except Exception as e:
             print(e)
-            error = {"error": "Something went wrong!! Try again later!"}
-            error = {"error": e}
+            # error = {"error": "Something went wrong!! Try again later!"}
+            #error = {"error": e}
 
-            return render_template("404.html", error=error)
+            return render_template("404.html", error=e)
     else:
         return render_template("index.html")
 
